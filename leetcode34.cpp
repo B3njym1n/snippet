298e909c6344b1f47ebc355f4ddcdc643ae65b3b
benjamin <benjaminyin@foxmail.com>
2023-03-07 12:57:13 +0800

    passed

--- leetcode34.cpp
+++ leetcode34.cpp
@@ -4,52 +4,54 @@
 
 using namespace std;
 
-int binarySearch2(vector<int> &list, int left, int right, int target )
-{
-  for (;left <= right;)
-    {
-      int mid = (left + right ) >> 1;
-      if (list[mid] == target)
-        {
-          return mid;
-        }
-      else if (list[mid] > target)
-        {
-          right = mid - 1;
-        }
-      else if (list[mid] < target)
-        {
-          left = mid + 1;
-        }
-    }
-  return -1;
-}
-
-
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1;
-        int one = binarySearch2(nums, left, right, target);
-        int first = one, last = one;
-        while(first > 0 && nums[first - 1] == nums[one])
-        {
-            first--;
-        }
-        while(last < right && nums[last + 1] == nums[one])
-        {
-            last++;
-        }
         vector<int> ret;
+        int first = bSearch(target, nums, true);
+        int last = bSearch(target, nums, false);
         ret.push_back(first);
         ret.push_back(last);
         return  ret;
     }
+
+  int bSearch(int target, vector<int>& nums, bool first)
+  {
+    int left = 0;
+    int right = nums.size() - 1;
+    int ret = -1;
+    while (left <= right)
+      {
+        int mid = (left + right) >> 1;
+        if (target < nums[mid])
+          {
+            right = mid - 1;
+          }
+        else if (target > nums[mid])
+          {
+            left = mid + 1; 
+          }
+        else
+          {
+            ret = mid;
+            if (first)
+              {
+                right = mid-1;
+              }
+            else
+              {
+                left = mid+1;
+              }
+          }
+      }
+    return ret; 
+  }
 };
 
 int main()
 {
     vector<int> l = {5,7,7,8,8,10};
+    // vector<int> l = {1};
     Solution s;
     vector<int> ret = s.searchRange(l, 8);
     cout << ret[0] << " " << ret[1] << endl;
